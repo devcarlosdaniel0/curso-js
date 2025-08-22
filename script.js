71ce8a141137d9b8646533e6fab3a6f08f5809a6
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-08-22 19:15:30 -0300

    booleanos

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let string2 = "Só as melhores"
 let num = 123;
 
 console.log("%s %s %s", string, string2, num);
+
+// booleanos
+
+let idade = 17
+let isMaiorIdade = idade >= 18 
+
+console.log(isMaiorIdade);
+
