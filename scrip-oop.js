0230032f722f7b3f326a83e24d8beaa73b61c5e6
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-08-27 17:18:15 -0300

    Classes

--- scrip-oop.js
+++ scrip-oop.js
@@ -5,3 +5,23 @@ const carro = {
 }
 
 console.log(carro);
+
+class Carro {
+    constructor(marca, ano, cor) {
+        this.marca = marca;
+        this.ano = ano;
+        this.cor = cor;
+    }
+
+    buzinar() {
+        return this.marca + " biii";
+    }
+}
+
+const celta = new Carro("Celta", 2002, "prateado"); 
+const gol = new Carro("Gol", 2013, "preto");
+console.log(celta);
+
+console.log("Marca do carro: " + gol.marca);
+console.log(gol.buzinar());
+console.log(celta.buzinar());
