11ac8c2e618ee33f0936d8227218c52a6baa4025
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-09-17 09:40:59 -0300

    back end pratice

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -5,6 +5,8 @@ import { removeFromCart } from "../data/cart.js";
 import { deliveryOptions } from "../data/deliveryOptions.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 
+import "../backend/backend-pratice.js";
+
 let cartSummaryHTML = "";
 
 cart.forEach((cartItem) => {
