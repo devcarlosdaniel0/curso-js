14a810b7f2fa4bc3e0a97e31c33741410daf5966
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-09-17 10:09:40 -0300

    adding the copy of code lesson 17

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,18 +1,25 @@
-export let cart = JSON.parse(localStorage.getItem("cart")) || [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-    deliveryOptionId: "1"
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-    deliveryOptionId: "2"
-  },
-];
+export let cart;
 
-function saveToLocalStorage() {
-  localStorage.setItem("cart", JSON.stringify(cart));
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function addToCart(productId) {
@@ -24,11 +31,17 @@ export function addToCart(productId) {
     }
   });
 
-  matchingItem
-    ? (matchingItem.quantity += 1)
-    : cart.push({ productId, quantity: 1, deliveryOptionId: "1" });
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: '1'
+    });
+  }
 
-  saveToLocalStorage();
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -42,5 +55,19 @@ export function removeFromCart(productId) {
 
   cart = newCart;
 
-  saveToLocalStorage();
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
 }
